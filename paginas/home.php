4f9d0e194d83cc35a52a28ccd5c7ab54bb0ca5f6
Mariana Luduena <marianabelenluduena@gmail.com>
2023-05-03 18:51:47 -0300

    Carrousel cambiado
    Se agregó una img más al carrousel, controles y un btn con el link a todos los productos.

--- paginas/home.php
+++ paginas/home.php
@@ -2,6 +2,15 @@
 
 <div id="carouselExampleFade" class="carousel slide carousel-fade d-none d-lg-block" data-bs-ride="carousel">
 
+    <div id="carouselExampleIndicators" class="carousel slide d-none d-lg-block" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+        </div>
+
         <div class="carousel-inner">
             <div class="carousel-item active" data-interval="100">
                 <img src="./img/carrousel/cartas__ps__4.jpg" class="d-block w-100 img-fluid img__carrousel" alt="Carta de póker flotando">
@@ -39,7 +48,28 @@
                 </div>
             </div>
 
-        
+            <div class="carousel-item" data-interval="1000">
+                <img src="./img/carrousel/sogas__ps.jpg" class="d-block w-100 img-fluid img__carrousel" alt="Manos sosteniendo una cuerda">
+                <div class="carousel-caption d-none d-md-block">
+
+                    <p class="box b1 animUno parrafo__carrousel">
+                        Conocé más
+                    </p>
+                    <!-- Link a los productos -->
+                   <a href="index.php?pag=productos&rama=todos"><button class="btn btn-primary form__btn">Ver productos</button></a>
+                </div>
+            </div>
+
+            <button class="carousel-control-prev d-none d-lg-block" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next d-none d-lg-block" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+
+        </div>
     </div>
 </div>
 
