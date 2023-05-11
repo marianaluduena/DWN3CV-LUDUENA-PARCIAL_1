81d3343356fcd55f60814a289efb39a360009c6b
Mariana Luduena <marianabelenluduena@gmail.com>
2023-05-10 23:13:14 -0300

    Página contacto
    Se borró código viejo y se arregló la semántica

--- paginas/contacto.php
+++ paginas/contacto.php
@@ -5,65 +5,6 @@
 
 <section class="row">
     <div class="container py-3">
-        <!-- CÓDIGO ORIGINAL NO BORRAR POR AHORA
-
-    <div class="container-fluid contacto__container">
-
-        <h2 id="contacto" class="my-4 my-md-5 text-md-center py-3">Contacto</h2>
-
-        <form method="post" class="row">
-
-            <div class="col-12 col-md-6 mb-3">
-                <div class="mb-3">
-                    <label for="nombre" class="form-label datos">Nombre</label>
-                    <input type="text" class="form-control" id="nombre" aria-describedby="nombre" placeholder="Ingresá tu nombre" required>
-                </div>
-            </div>
-
-            <div class="col-12 col-md-6 mb-3">
-                <div class="mb-3">
-                    <label for="apellido" class="form-label datos">Apellido</label>
-                    <input type="text" class="form-control" id="apellido" aria-describedby="apellido" placeholder="Ingresá tu apellido" required>
-                </div>
-            </div>
-
-            <div class="col-12 col-md-6 mb-3">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label datos">Email</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="email" placeholder="Ingresá tu email" required>
-                </div>
-            </div>
-
-            <div class="col-12 col-md-6 mb-3">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label datos">Teléfono</label>
-                    <input type="number" class="form-control" id="telefono" aria-describedby="telefono" placeholder="Número de teléfono" required>
-                </div>
-            </div>
-
-            <div id="opciones">
-                <div class="col-12 col-md-6 mb-3">
-                    <label for="magiaFav" class="form-label datos">Tipo de magia</label>
-                    <select class="form-select" name="magiaFav" id="magiaFav">
-                        <option value="1">Close up</option>
-                        <option value="2">Escapismo</option>
-                        <option value="3">Manipulación</option>
-                        <option value="4">Magia de escenario</option>
-                    </select>
-                </div>
-            </div>
-
-            <label for="comentarios" class="form-label"> </label>
-
-            <div class="container__form__textarea">
-                <textarea id="comentarios" name="comentarios" placeholder="Escribí aquí tu consulta o comentario" rows="3"></textarea>
-            </div>
-
-            <button type="submit" class="btn btn-primary form__btn">Enviar</button>
-        </form>
-    </div> -->
-
-
         <h2 class="titulo__especial my-4 text-md-center py-3">Contacto</h2>
 
         <div class="row justify-content-center">
@@ -83,10 +24,10 @@
                     </div>
                     <div class="col-md-6">
                         <label for="nroTel" class="form-label datos">Teléfono *</label>
-                        <input type="number" class="form-control my-1 input__form" id="nroTel" name="nroTel" aria-describedby="telefono" placeholder="Número de teléfono" required>
+                        <input type="number" class="form-control my-1 input__form" id="nroTel" name="nroTel" aria-describedby="nroTel" placeholder="Número de teléfono" required>
                     </div>
                     <div class="col-md-6">
-                        <label for="inputState" class="form-label datos">Año de nacimiento</label>
+                        <label for="anio__nacimiento" class="form-label datos">Año de nacimiento</label>
                         <select class="form-select my-1 input__form" id="anio__nacimiento" name="anio__nacimiento" required>
                             <option value=""> Año de nacimiento: * </option>
 
